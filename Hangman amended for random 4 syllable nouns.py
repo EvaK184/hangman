42b1c39f04480cad4efd4f0b4ab899c0bdf07d9e
EvaK184 <evageliakyriakidou@gmail.com>
2024-11-18 18:22:18 +0000

    My first commit

---
+++ Hangman amended for random 4 syllable nouns.py
@@ -0,0 +1,54 @@
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
+
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters \
+                   .index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n"
+              .join(stages[0: e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n"
+              .join(stages[0: \
+              wrong]))
+        print("You lose! It was {}."
+              .format(word))
+
+import random
+nouns2=[]
+with open("nouns.txt", "r") as nouns:
+ lines = nouns.readlines()
+ for l in lines:
+  words=l.split("\n")
+  nouns2.append(words[0])
+                
+hangman(random.choice(nouns2))
